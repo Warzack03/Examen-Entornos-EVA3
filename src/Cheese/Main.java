af85f43a762a957ddf875e04cb5b8f5dc9def0b6
Warzack03 <aaronbm03@gmail.com>
2022-06-01 11:13:41 +0200

    COMMIT MASTER MAIN

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -12,8 +12,30 @@ package Cheese;
  */
 public class Main {
     
+    ListaProductos miLista = new ListaProductos();
+    
+    Producto yogur = new Producto("001", "desnatada", 70, 1.6);
+    Producto queso = new Producto("002", "semi-desnatada", 90, 2.6);
+    Producto leche = new Producto("003", "entera", 80, 0.8);
+    
      public static void main(String[] args) {
     
-         System.out.println("1º Gdam: Entornos de desarrollo");
+         System.out.println("1º GDAM: Entornos de desarrollo");
+         
+         // ME SALGO DEL MAIN PARA QUE NO ME DE ERRORES POR SER UN MÉTODO STATIC
+         
+         Main m = new Main();
+         m.test();
     }
+     
+     public void test()
+     {
+         miLista.addProducto(yogur);
+         miLista.addProducto(queso);
+         miLista.addProducto(leche);
+         
+         yogur.imprime_etiqueta();
+         queso.imprime_etiqueta();
+         leche.imprime_etiqueta();
+     }
 }
